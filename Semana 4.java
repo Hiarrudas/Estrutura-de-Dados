643a45e34480be288fa84939251ebf8029e012b9
Hiarruda <166509686+Hiarrudas@users.noreply.github.com>
2025-09-04 23:28:39 -0300

    Add files via upload

---
+++ Semana 4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Menu{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int opcao;
+
+        do{
+            System.out.println("\n===== MENU DE EXERCÍCIOS COM ARRAYS ===== \n Escolha uma das opções a seguir:");
+            System.out.println("\n 1) Contar Pares e Ímpares ");
+            System.out.println("\n 2) Procurar número no array");
+            System.out.println("\n 3) Remover nome");
+            System.out.println("\n 4) Contar repetições de números");
+            System.out.println("\n 5) Ordenar com Bubble Sort");
+            System.out.println("\n 0) Sair");
+            System.out.println("\n Use o número para escolher a operação");
+
+        }
+    }
+}
